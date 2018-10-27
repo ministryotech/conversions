108f5b923a724d52922ff0e4a70bcd1653c3ed39
Keith Jackson <keith@ministryotech.co.uk>
2018-10-27 21:27:06 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,25 @@
+A selection of extension methods to provide easy conversion between some of the primary data types.
+
+- **Boolean.ToInt32** - Converts a boolean value to an integer with optional parameters to specify the translated true and false values.
+- **Boolean.ToString** - Takes a defined type of conversion to convert a bool value into either True / False, Yes / No or Y / N strings.
+- **Int32.ToBoolean** - Converts an integer greater than 0 into true.
+- **String.ToBoolean** - Converts any valid string to a representative boolean value. The conversion is case insensitive and will take either 1 / 0 / -1, Yes / No, True / False or Y / N as valid input strings.
+
+## The Ministry of Technology Open Source Products
+Welcome to The Ministry of Technology open source products. All open source Ministry of Technology products are distributed under the MIT License for maximum re-usability. Details on more of our products and services can be found on our website at http://www.minotech.co.uk
+
+Our other open source repositories can be found here...
+
+* [https://github.com/ministryotech](https://github.com/ministryotech)
+* [https://github.com/tiefling](https://github.com/tiefling)
+
+### Where can I get it?
+You can download the package for this project from any of the following package managers...
+
+- **NUGET** - [https://www.nuget.org/packages/Ministry.Conversions](https://www.nuget.org/packages/Ministry.Conversions)
+
+### Contribution guidelines
+If you would like to contribute to the project, please contact me.
+
+### Who do I talk to?
+* Keith Jackson - keith@minotech.co.uk
---
+++ src/Ministry.Conversions/Conversions.cs
@@ -0,0 +1,123 @@
+﻿// Copyright (c) 2018 Minotech Ltd.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files
+// (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, 
+// publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do 
+// so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF 
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE 
+// FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION 
+// WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+using System;
+
+// ReSharper disable once CheckNamespace
+namespace Ministry
+{
+    /// <summary>
+    /// A static class of methods to convert types which can be used directly or as extensions.
+    /// </summary>
+    public static class Conversions
+    {
+        /// <summary>
+        /// Converts a boolean to an integer value of a specified type.
+        /// </summary>
+        /// <param name="value">The boolean value to convert from.</param>
+        /// <param name="trueValue">The true value.</param>
+        /// <param name="falseValue">The false value.</param>
+        /// <returns>
+        /// The appropriate integer value.
+        /// </returns>
+        public static int ToInt32(this bool value, int trueValue = 1, int falseValue = 0) 
+            => value ? trueValue : falseValue;
+
+        /// <summary>
+        /// Converts a boolean to an integer value of a specified type.
+        /// </summary>
+        /// <param name="value">The boolean value to convert from.</param>
+        /// <param name="trueValue">The integer value to return if the boolean value is true.</param>
+        /// <param name="falseValue">The false value.</param>
+        /// <returns>
+        /// The appropriate integer value.
+        /// </returns>
+        public static int? ToInt32(this bool? value, int trueValue = 1, int falseValue = 0) 
+            => value?.ToInt32(trueValue, falseValue);
+
+        /// <summary>
+        /// Converts a boolean to a string value of a specified type.
+        /// </summary>
+        /// <param name="value">The boolean value to convert from.</param>
+        /// <param name="stringType">Type of the string.</param>
+        /// <returns>
+        /// The appropriate string value.
+        /// </returns>
+        /// <remarks>
+        /// To convert to 1/0 call ToInt().
+        /// </remarks>
+        public static string ToString(this bool value, BooleanToStringConversionType stringType)
+        {
+            switch (stringType)
+            {
+                case BooleanToStringConversionType.TrueOrFalse:
+                    return value ? "True" : "False";
+                case BooleanToStringConversionType.YesOrNo:
+                    return value ? "Yes" : "No";
+                case BooleanToStringConversionType.YOrN:
+                    return value ? "Y" : "N";
+                default:
+                    return string.Empty;
+            }
+        }
+
+        /// <summary>
+        /// Converts a boolean to a string value of a specified type.
+        /// </summary>
+        /// <param name="value">The boolean value to convert from.</param>
+        /// <param name="stringType">Type of the string.</param>
+        /// <returns>
+        /// The appropriate string value.
+        /// </returns>
+        /// <remarks>
+        /// To convert to 1/0 call ToInt().
+        /// </remarks>
+        public static string ToString(this bool? value, BooleanToStringConversionType stringType) 
+            => value.HasValue ? value.Value.ToString(stringType) : string.Empty;
+
+        /// <summary>
+        /// Converts an integer to it's boolean value.
+        /// </summary>
+        /// <param name="value">The integer to convert from.</param>
+        /// <returns>The appropriate boolean value.</returns>
+        /// <remarks>Converts 0 False, all else True.</remarks>
+        public static bool ToBoolean(this int value) => value > 0;
+
+        /// <summary>
+        /// Converts a string to it's boolean value.
+        /// </summary>
+        /// <param name="value">The string to convert from.</param>
+        /// <returns>The appropriate boolean value.</returns>
+        /// <remarks>Converts 1/0, True/False, Yes/No, etc. Returns nothing otherwise.</remarks>
+        /// <exception cref="System.InvalidCastException">Thrown if the string cannot be transformed into a boolean value.</exception>
+        public static bool ToBoolean(this string value)
+        {
+            switch (value.Trim().ToUpper())
+            {
+                case "1":
+                case "YES":
+                case "TRUE":
+                case "Y":
+                    return true;
+                case "0":
+                case "-1":
+                case "NO":
+                case "FALSE":
+                case "N":
+                    return false;
+                default:
+                    throw new InvalidCastException("The object passed cannot be converted");
+            }
+        }
+    }
+}
